99bb92b38be491f8bc46edc78c71e41defd429a7
Prashanta <kandep2@mcmaster.ca>
2025-01-15 14:55:49 -0500

    feat: initial setup, checks for -i flag, changed System.out to log.info

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Main.java
@@ -5,32 +5,51 @@ import java.io.File;
 import java.io.FileReader;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
+import org.apache.commons.cli.*;
 
 public class Main {
 
     private static final Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
-        System.out.println("** Starting Maze Runner");
+        logger.info("** Starting Maze Runner");
+
+        // create Options object
+        Options options = new Options();
+        // add t option
+        options.addOption("i", "flag", false, "load maze");
+
         try {
-            System.out.println("**** Reading the maze from file " + args[0]);
-            BufferedReader reader = new BufferedReader(new FileReader(args[0]));
-            String line;
-            while ((line = reader.readLine()) != null) {
-                for (int idx = 0; idx < line.length(); idx++) {
-                    if (line.charAt(idx) == '#') {
-                        System.out.print("WALL ");
-                    } else if (line.charAt(idx) == ' ') {
-                        System.out.print("PASS ");
+            // create parser for command line arguments
+            CommandLineParser parser = new DefaultParser();
+            CommandLine commandLine = parser.parse(options, args);
+
+            if (commandLine.hasOption("i")) { // if it has the -i flag
+                // Conntinue
+
+                logger.trace("**** Reading the maze from file " + args[1]);
+                BufferedReader reader = new BufferedReader(new FileReader(args[1]));
+                String line;
+                while ((line = reader.readLine()) != null) {
+                    for (int idx = 0; idx < line.length(); idx++) {
+                        if (line.charAt(idx) == '#') {
+                            logger.debug("WALL ");
+                        } else if (line.charAt(idx) == ' ') {
+                            logger.debug("PASS ");
+                        }
                     }
+                    logger.debug(System.lineSeparator());
                 }
-                System.out.print(System.lineSeparator());
+            } else {
+                // it does not have the -i flag
+                throw new Exception("No Maze Detected");
             }
-        } catch(Exception e) {
-            System.err.println("/!\\ An error has occured /!\\");
+
+        } catch (Exception e) {
+            logger.error("/!\\ An error has occured /!\\");
         }
-        System.out.println("**** Computing path");
-        System.out.println("PATH NOT COMPUTED");
-        System.out.println("** End of MazeRunner");
+        logger.trace("**** Computing path");
+        logger.error("PATH NOT COMPUTED");
+        logger.info("** End of MazeRunner");
     }
 }
