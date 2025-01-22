d9e210a8cee58b2da5be0aaebe9b7eeae4da3f1e
Prashanta <kandep2@mcmaster.ca>
2025-01-21 19:21:48 -0500

    feat: Move forward method created, increments or decrements postion depending on direction player is facing (close A1-MazeRunner#<2>)

--- src/main/java/ca/mcmaster/se2aa4/mazerunner/Player.java
+++ src/main/java/ca/mcmaster/se2aa4/mazerunner/Player.java
@@ -4,6 +4,7 @@ public class Player {
     // Attributes
     private int position_x;
     private int position_y;
+    private char direction = 'E'; // Start with direction always facing East
 
     // Contructor
     public Player(int position_x, int position_y) {
@@ -16,13 +17,44 @@ public class Player {
         return position_x;
     }
 
+    public void setX(int position_x) {
+        this.position_x = position_x;
+    }
+
     public int getY() {
         return position_y;
+    }
+
+    public void setY(int position_y) {
+        this.position_y = position_y;
+    }
+
+    public char getDirection() {
+        return direction;
+    }
+
+    public void setDirection(char direction) {
+        this.direction = direction;
     }
 
     // Methods
 
     public void moveForward() {
+        if (direction == 'N') {
+            // Facing North
+            position_y++; // increase y by one
+        } else if (direction == 'E') {
+            // Facing East
+            position_x++;
+        } else if (direction == 'S') {
+            // Facing South
+            position_y--;
+        } else if (direction == 'W') {
+            // Facing West
+            position_x--;
+        } else {
+            // Error no movement
+        }
 
     }
 
